0e21bbfffbcbe371915d1a4f67db688266165844
VanoKi <kirsvano@gmail.com>
2025-04-17 18:16:30 +0300

    fix: tabs

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ export type Task = {
 
 export const App = () => {
   const tasks: Task[] = [
-    { id: 1, title: 'HTML&CSS', isDone: true },
-    { id: 2, title: 'JS', isDone: true },
-    { id: 3, title: 'ReactJS', isDone: false },
-    { id: 4, title: 'Redux', isDone: false },
-    { id: 5, title: 'Typescript', isDone: false },
-    { id: 6, title: 'RTK query', isDone: false },
+    {id: 1, title: 'HTML&CSS', isDone: true},
+    {id: 2, title: 'JS', isDone: true},
+    {id: 3, title: 'ReactJS', isDone: false},
+    {id: 4, title: 'Redux', isDone: false},
+    {id: 5, title: 'Typescript', isDone: false},
+    {id: 6, title: 'RTK query', isDone: false},
   ]
 
   return (
-      <div className="app">
-        <TodolistItem title="What to learn" tasks={tasks} truck2={crypto.randomUUID()}/>
-        <TodolistItem title="What to learn 2" tasks={tasks} truck2={crypto.randomUUID()}/>
-        <TodolistItem title="What to learn 3" tasks={tasks} truck2={crypto.randomUUID()}/>
-      </div>
+    <div className="app">
+      <TodolistItem title="What to learn" tasks={tasks} truck2={crypto.randomUUID()}/>
+      <TodolistItem title="What to learn 2" tasks={tasks} truck2={crypto.randomUUID()}/>
+      <TodolistItem title="What to learn 3" tasks={tasks} truck2={crypto.randomUUID()}/>
+    </div>
   )
 }
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -4,37 +4,37 @@ import {Button} from './Button'
 type Props = {
   title: string
   tasks: Task[]
-    truck2: string
+  truck2: string
 }
 
 export const TodolistItem = ({title, tasks, truck2}: Props) => {
-    return (
+  return (
+    <div>
+      <h3>{title}</h3>
+      {<p>{truck2}</p>}
       <div>
-        <h3>{title}</h3>
-          {<p>{truck2}</p>}
-          <div>
-          <input/>
-          <Button title={'+'} />
-        </div>
-        {tasks.length === 0 ? (
-            <p>Тасок нет</p>
-        ) : (
-            <ul>
-              {tasks.map(task => {
-                return (
-                    <li key={task.id}>
-                      <input type="checkbox" checked={task.isDone} />
-                      <span>{task.title}</span>
-                    </li>
-                )
-              })}
-            </ul>
-        )}
-        <div>
-          <Button title={'All'} />
-          <Button title={'Active'} />
-          <Button title={'Completed'} />
-        </div>
+        <input/>
+        <Button title={'+'}/>
       </div>
+      {tasks.length === 0 ? (
+        <p>Тасок нет</p>
+      ) : (
+        <ul>
+          {tasks.map(task => {
+            return (
+              <li key={task.id}>
+                <input type="checkbox" checked={task.isDone}/>
+                <span>{task.title}</span>
+              </li>
+            )
+          })}
+        </ul>
+      )}
+      <div>
+        <Button title={'All'}/>
+        <Button title={'Active'}/>
+        <Button title={'Completed'}/>
+      </div>
+    </div>
   )
 }
