e9b7c62a12ad1724e37c1ace7aff3d7b211a43fc
VanoKi <kirsvano@gmail.com>
2025-05-11 14:46:22 +0300

    feat: opacity

--- src/Task.tsx
+++ src/Task.tsx
@@ -14,7 +14,7 @@ export const Task = ({id, title, isDone, removeTask, changeIsDone}: Props) => {
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {changeIsDone(id, e.currentTarget.checked)}
 
   return (
-    <li>
+    <li className={isDone ? 'is-done' : ''}>
       <input
         type="checkbox"
         checked={isDone}
