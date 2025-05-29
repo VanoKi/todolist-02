4bad107096852033c6650937db502b346cc19efc
VanoKi <kirsvano@gmail.com>
2025-05-29 14:50:13 +0300

    fix: imports

--- src/components/MyAppBar.tsx
+++ src/components/MyAppBar.tsx
@@ -1,9 +1,9 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import {MenuBtn} from "./MenuBtn.tsx";
 
 export const MyAppBar = () => {
   return (
@@ -21,9 +21,9 @@ export const MyAppBar = () => {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           News
         </Typography>
-        <Button color="inherit">Login</Button>
-        <Button color="inherit">LogOut</Button>
-        <Button color="inherit">FAQ</Button>
+        <MenuBtn color="inherit">Login</MenuBtn>
+        <MenuBtn color="inherit">LogOut</MenuBtn>
+        <MenuBtn color="inherit">FAQ</MenuBtn>
       </Toolbar>
     </AppBar>
   );
