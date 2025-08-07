8da47acb7fe6447d89c5fa3a327b35ef41e45afd
VanoKi <kirsvano@gmail.com>
2025-08-07 18:56:43 +0300

    feat: add case create_task

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -17,10 +17,16 @@ export const tasksReducer = (state: TasksState = initialState, action: Actions):
       const [todolistId, taskId] = action.payload
       return {...state, [todolistId] : state[todolistId].filter(task => task.id !== taskId)}
     }
+    case 'create_task': {
+      const [todolistId, newTitle] = action.payload
+      return {...state, [todolistId]:[newTitle, ...state[todolistId]]}
+    }
     default:
       return state
   }
 }
 export const deleteTaskAC = ({todolistId, taskId}:{todolistId:string, taskId:string}) => {return {type: 'remove_task', payload: {todolistId, taskId}}}
+export const createTaskAC = ({todolistId, newTitle}:{todolistId:string, newTitle:string}) => {return {type: 'create_task', payload: {todolistId, newTitle}}}
 export type DeleteTaskAC = ReturnType<typeof deleteTaskAC>
-type Actions = CreateTodolistAction | DeleteTodolistAction | DeleteTaskAC
+export type CreateTaskAC = ReturnType<typeof createTaskAC>
+type Actions = CreateTodolistAction | DeleteTodolistAction | DeleteTaskAC | CreateTaskAC
