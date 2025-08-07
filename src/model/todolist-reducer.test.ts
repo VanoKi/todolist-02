909ff1974e8adb5d478766889d6bef469b8e0f06
VanoKi <kirsvano@gmail.com>
2025-08-07 16:49:24 +0300

    fix: beforeEach

--- src/model/todolist-reducer.test.ts
+++ src/model/todolist-reducer.test.ts
@@ -1,36 +1,30 @@
-import {v1} from 'uuid'
-import { expect, test } from 'vitest'
-import type {TodolistType} from '../App'
-import {createTodolistAC, deleteTodolistAC, todolistReducer} from './todolist-reducer'
+import { beforeEach, expect, test } from 'vitest'
+import {TodolistType} from "../App.tsx";
+import { v1 } from 'uuid';
+import {createTodolistAC, deleteTodolistAC, todolistReducer} from "./todolist-reducer.ts";
+
+let todolistId1: string
+let todolistId2: string
+let startState: TodolistType[] = []
+
+beforeEach(() => {
+  todolistId1 = v1()
+  todolistId2 = v1()
+
+  startState = [
+    { id: todolistId1, title: 'What to learn', filter: 'all' },
+    { id: todolistId2, title: 'What to buy', filter: 'all' },
+  ]
+})
 
 test('correct todolist should be deleted', () => {
-  const todolistId1 = v1()
-  const todolistId2 = v1()
-
-  // 1. Стартовый state
-  const startState: TodolistType[] = [
-    {id: todolistId1, title: 'What to learn', filter: 'all'},
-    {id: todolistId2, title: 'What to buy', filter: 'all'},
-  ]
-
-  // 2. Действие
   const endState = todolistReducer(startState, deleteTodolistAC(todolistId1))
 
-  // 3. Проверка, что действие измененило state соответствующим образом
-  // в массиве останется один тудулист
   expect(endState.length).toBe(1)
-  // удалится нужный тудулист, не любой
   expect(endState[0].id).toBe(todolistId2)
 })
+
 test('correct todolist should be created', () => {
-  const todolistId1 = v1()
-  const todolistId2 = v1()
-
-  const startState: TodolistType[] = [
-    {id: todolistId1, title: 'What to learn', filter: 'all'},
-    {id: todolistId2, title: 'What to buy', filter: 'all'},
-  ]
-
   const title = 'New todolist'
   const endState = todolistReducer(startState, createTodolistAC(title))
 
