5a91e8bd556ec8e551a6b004c445409ad1b104cd
VanoKi <kirsvano@gmail.com>
2025-08-07 18:09:27 +0300

    feat: add to task-reducer createTodolistAC

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -1,15 +1,16 @@
 import type {TasksState} from '../App'
+import {CreateTodolistAction} from "./todolist-reducer.ts";
 
 const initialState: TasksState = {}
 
 export const tasksReducer = (state: TasksState = initialState, action: Actions): TasksState => {
   switch (action.type) {
-    case '': {
-      return state
+    case 'create_todolist': {
+      return {...state, [action.payload.id]:[]}
     }
     default:
       return state
   }
 }
 
-type Actions = any
+type Actions = CreateTodolistAction
